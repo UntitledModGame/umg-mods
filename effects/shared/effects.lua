a2a80f4dbfd6f4da4329318f89fe0f2d812cd59a
Oli <OliverWDGarrett@gmail.com>
2023-10-23 14:15:40 +1300

    wired it all up

--- effects/shared/effects.lua
+++ effects/shared/effects.lua
@@ -1,6 +1,9 @@
 
 local EffectManager = require("shared.effect_manager")
 
+local EventEffectHandler = require("shared.handlers.event_effect_handler")
+local QuestionEffectHandler = require("shared.handlers.question_effect_handler")
+
 
 
 local effects = {}
@@ -29,8 +32,25 @@ end
 
 
 
-function effects.
+function effects.tryCallEvent(ent, eventName, ...)
+    if ent.effects then
+        local eventEH = ent.effects:getEffectHandler(EventEffectHandler)
+        if eventEH then
+            eventEH:call(eventName, ...)
+        end
+    end
+end
 
+
+function effects.tryAnswerQuestion(ent, questionName, ...)
+    do error("nyi") end
+    if ent.effects then
+        local questionEH = ent.effects:getEffectHandler(QuestionEffectHandler)
+        if questionEH then
+            questionEH:ask(questionName, ...)
+        end
+    end
+end
 
 
 return effects
