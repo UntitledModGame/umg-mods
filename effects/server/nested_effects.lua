56b9fb6e43311a0ef809aaa7c3797137c542ae5b
Oli <OliverWDGarrett@gmail.com>
2023-10-26 14:39:08 +1300

    added nestedEffects, did a bunch of renaming

---
+++ effects/server/nested_effects.lua
@@ -0,0 +1,31 @@
+
+--[[
+
+Nested effects
+
+This basically just allows us to compose effects nicely,
+which is cool.
+
+
+]]
+
+local effects = require("shared.effects")
+
+
+umg.on("effects:effectAdded", function(effectEnt, ent)
+    if effectEnt.nestedEffect then
+        for _, nestedEnt in ipairs(ent.nestedEffect) do
+            effects.addEffect(ent, nestedEnt)
+        end
+    end
+end)
+
+
+umg.on("effects:effectRemoved", function(effectEnt, ent)
+    if ent.nestedEffect then
+        for _, nestedEnt in ipairs(ent.nestedEffect) do
+            effects.removeEffect(ent, nestedEnt)
+        end
+    end
+end)
+
