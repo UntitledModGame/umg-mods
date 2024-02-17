0968893c4c2569d732e0455a226eadeac3fef6d1
Oli <OliverWDGarrett@gmail.com>
2024-02-17 13:27:44 +1300

    controller is now auto-synced.

--- positions/shared/options.lua
+++ positions/shared/options.lua
@@ -3,10 +3,13 @@
 local options = {
     DEFAULT_SPEED = 100,
 
-    SYNC_LEIGHWAY = 3, --[[
+    SYNC_LEIGHWAY = 40, --[[
         players have X times sync leighway. This essentially means that
         hacked clients can technically move-hack to go X times as fast;
         (but on the positive side, legit clients won't be lagged backwards.)
+
+        (We are just setting it to be super high for now, because i
+            CBA dealing with lagback.)
     ]]
 
     DEFAULT_FRICTION = 6;
---
+++ sync/shared/controller.lua
@@ -0,0 +1,7 @@
+
+local autoSyncComponent = require("shared.auto_sync_component")
+autoSyncComponent("controller", {
+    type = "string",
+    syncWhenNil = true
+})
+
