c466b49f00587b472cc309126797021959a0c631
Oli <OliverWDGarrett@gmail.com>
2024-03-21 14:05:55 +1300

    Deleted `modern` mod (unused and old)

--- modern/entities/sports/football_goal_down.lua
+++
@@ -1,9 +0,0 @@
-
-
-
-return {
-    image = "ME_Singles_School_16x16_Soccer_Net_1",
-
-    initXY = true
-}
-
--- modern/entities/sports/football_goal_up.lua
+++
@@ -1,8 +0,0 @@
-
-
-return {
-    image = "ME_Singles_School_16x16_Soccer_Net_3",
-
-    initXY = true
-}
-
