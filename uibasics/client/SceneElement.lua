b77d9fec5c0f901047ea09ee4b73786b2b774e2b
Oli <OliverWDGarrett@gmail.com>
2024-04-10 22:38:52 +1200

    Put legacy methods back

--- uibasics/client/SceneElement.lua
+++ uibasics/client/SceneElement.lua
@@ -14,49 +14,6 @@ end
 
 
 
-local function getElement(elem_or_ent)
-    local elem = elem_or_ent
-    if umg.exists(elem_or_ent) then
-        if not elem_or_ent.ui then
-            return nil, "UI entity needs .ui component!"
-        end
-        elem = elem_or_ent.ui.element
-    end
-    return elem
-end
-
-local function ensureElement(elem_or_ent)
-    local elem, er = getElement(elem_or_ent)
-    if not elem then
-        umg.melt(er)
-    end
-    return elem
-end
-
-
-function Scene:open(elem_or_ent)
-    local elem = ensureElement(elem_or_ent)
-    assert(elem:getDefaultRegion(), "Element needs a default region!")
-    self:addChild(elem)
-end
-
-
-function Scene:close(elem_or_ent)
-    local elem = ensureElement(elem_or_ent)
-    self:removeChild(elem)
-end
-
-
-function Scene:isOpen(elem_or_ent)
-    local elem = getElement(elem_or_ent)
-    return self:hasChild(elem)
-end
-
-
-function Scene:getOpenElements()
-    return self:getChildren()
-end
-
 
 
 
--- uibasics/shared/uibasics_exports.lua
+++ uibasics/shared/uibasics_exports.lua
@@ -13,19 +13,51 @@ function ui.basics.getSceneRegion()
     return ui.Region(0,0,love.graphics.getDimensions())
 end
 
-function ui.basics.is
 
-
-function ui.basics.isOpen(ent)
-    if ent.ui and ent.ui.element then
-        local elem = ent.ui.element
-        return scene:hasChild(elem)
+local function getElement(elem_or_ent)
+    local elem = elem_or_ent
+    if umg.exists(elem_or_ent) then
+        if not elem_or_ent.ui then
+            return nil, "UI entity needs .ui component!"
+        end
+        elem = elem_or_ent.ui.element
     end
+    return elem
 end
+
+local function ensureElement(elem_or_ent)
+    local elem, er = getElement(elem_or_ent)
+    if not elem then
+        umg.melt(er)
+    end
+    return elem
+end
+
+
+function ui.basics.open(elem_or_ent)
+    local elem = ensureElement(elem_or_ent)
+    assert(elem:getDefaultRegion(), "Element needs a default region!")
+    scene:addChild(elem)
+end
+
+
+function ui.basics.close(elem_or_ent)
+    local elem = ensureElement(elem_or_ent)
+    scene:removeChild(elem)
+end
+
+
+function ui.basics.isOpen(elem_or_ent)
+    local elem = getElement(elem_or_ent)
+    return scene:hasChild(elem)
+end
+
 
 function ui.basics.getOpenElements()
     return scene:getChildren()
 end
+
+
 
 ui.basics.SCENE = scene
 
