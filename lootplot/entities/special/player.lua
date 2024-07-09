da0c420a792801d93912276960eebf7540b38347
Oli <OliverWDGarrett@gmail.com>
2024-07-09 13:53:39 +1200

    small changes

--- lootplot/entities/special/player.lua
+++ lootplot/entities/special/player.lua
@@ -3,13 +3,10 @@
 local function initPlayer(ent, clientId)
     assert(clientId,"?")
     ent.controller = clientId
-    ent.money = 0
 end
 
 return umg.defineEntityType("lootplot:player", {
     speed = 600,
-    topdownControl = {};
-    initVxVy = true,
     cameraFollow = true;
 
     image = "player",
