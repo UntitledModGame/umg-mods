74625cbd9d88ef777fc5e89b0bff078c7d712297
Oli <OliverWDGarrett@gmail.com>
2024-08-01 19:57:48 +1200

    Fixed sync.syncComponent

--- sync/shared/manual_sync_component.lua
+++ sync/shared/manual_sync_component.lua
@@ -5,7 +5,6 @@ sync.syncComponent(ent, compName)
 
 
 Manually syncs a component on serverside.
-Uses string -> id compression to compress the component name.
 (Doesn't care about delta compression or nils)
 
 
@@ -13,43 +12,22 @@ Uses string -> id compression to compress the component name.
 
 
 
-local currentId = 0
-
-local componentIdCache = {--[[
-    [id] -> compName
-    [compName] -> id
-
-    compresses component names to ids, for easy access.
-]]}
-
-
 umg.definePacket("sync:syncComponent", {
-    --          entity  comp_id  serialization-data
-    typelist = {"entity", "number", "string"}
+    --          entity  comp_name  serialization-data
+    typelist = {"entity", "string", "string"}
 })
-
-umg.definePacket("sync:setSyncComponentCache", {
-    --         json-data
-    typelist = {"string"}
-})
-
 
 
 
 
 if client then
 
-client.on("sync:setSyncComponentCache", function(cacheData)
-    local cache = umg.deserialize(cacheData)
-    componentIdCache = cache
-end)
-
-client.on("sync:syncComponent", function(ent, id, compValue)
-    local compName = componentIdCache[id]
-    if compName then
+client.on("sync:syncComponent", function(ent, compName, compData)
+    local compValue, err = umg.deserializeVolatile(compData)
+    if compValue and not err then
         ent[compName] = compValue
     else
-        print("[sync.syncComponent] WARNING: Unknown component id")
+        umg.log.fatal(err)
     end
 end)
 
@@ -61,45 +39,12 @@ end
 
 if server then
 
-local function updateCompIdCache()
-    --[[
-        dear future Oli:
-        pls dont hate me if there's a desync because of this
-    ]]
-    server.broadcast("sync:setSyncComponentCache", umg.serialize(componentIdCache))
-end
-
-umg.on("@playerJoin", function()
-    -- whenever a new player joins, send over the component id cache.
-    -- (Yes it's shit, but who cares)
-    updateCompIdCache()
-end)
-
-
-local function addEntry(compName)
-    currentId = currentId + 1
-    local compId = currentId
-    componentIdCache[compId] = compName
-    componentIdCache[compName] = compId
-end
-
-
-local function getComponentId(compName)
-    if not componentIdCache[compName] then
-        addEntry(compName)
-        updateCompIdCache()
-    end
-    return componentIdCache[compName]
-end
-
-
-
 local syncComponentTc = typecheck.assert("entity", "string")
 local function syncComponent(ent, compName)
     syncComponentTc(ent, compName)
 
-    local id = getComponentId(compName)
-    server.broadcast("sync:syncComponent", ent, id, ent[compName])
+    local data = umg.serializeVolatile(ent[compName])
+    server.broadcast("sync:syncComponent", ent, compName, data)
 end
 
 return syncComponent
