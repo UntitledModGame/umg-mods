cc8c0fc110c1c4725d1982f3e4c6c4dec16eae3a
Oli <OliverWDGarrett@gmail.com>
2024-08-09 18:33:22 +1200

    Added temporary balance changes

---
+++ lootplot.content.s0/entities/items/basic_remove_later/temporary.lua
@@ -0,0 +1,58 @@
+
+
+--[[
+
+Remove these later.
+
+these are temporary items, and we should remove them before public demo.
+
+
+
+
+]]
+
+lp.defineItem("lootplot.content.s0:coconut", {
+    image = "coconut",
+
+    name = localization.localize("Coconut"),
+    baseBuyPrice = 2,
+
+    rarity = lp.rarities.COMMON,
+    baseTraits = {},
+
+    basePointsGenerated = 2,
+})
+
+
+
+lp.defineItem("lootplot.content.s0:pear", {
+    image = "pear",
+
+    name = localization.localize("Pear"),
+    baseBuyPrice = 3,
+
+    rarity = lp.rarities.UNCOMMON,
+    baseTraits = {},
+
+    doomCount = 5,
+    basePointsGenerated = 1,
+    baseMoneyGenerated = 1,
+})
+
+
+
+lp.defineItem("lootplot.content.s0:cherry", {
+    image = "cherry",
+
+    name = localization.localize("Cherry"),
+    baseBuyPrice = 3,
+
+    rarity = lp.rarities.RARE,
+    baseTraits = {},
+
+    doomCount = 5,
+    basePointsGenerated = 4,
+})
+
+
+
