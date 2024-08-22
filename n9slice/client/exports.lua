08e5a6aed184210037b84a3bd115adca19369296
Oli <OliverWDGarrett@gmail.com>
2024-08-22 12:58:52 +1200

    removed/simplified n9slice api

--- n9slice/client/exports.lua
+++ n9slice/client/exports.lua
@@ -5,9 +5,12 @@ local n9slice = {}
 if false then _G.n9slice = n9slice end
 
 
--- Direct access to NPad93's 9-patch slice library.
--- If `n9slice` high-level function doesn't suit your needs, access the library functions directly here.
-n9slice.n9p = n9p
+--[[
+In future:
+if we really need to, we can re-expose n9p.
+Right now, API is too complex tho.
+]]
+-- n9slice.n9p = n9p
 
 
 ---@alias n9slice.StretchType
@@ -53,83 +56,6 @@ function n9slice.new(args)
 end
 
 
-local loadFromImageQuadTc = typecheck.assert("love:Texture|love:ImageData", "love:Quad?", "table?")
-
----@param texture love.Texture|love.ImageData Texture atlas.
----@param quad love.Quad|nil Subregion of the texture atlas to consider or `nil` for the whole texture atlas.
----@param settings n9slice.settings? Additional settings. See the `n9slice.settings` type for more information.
-function n9slice.loadFromImageQuad(texture, quad, settings)
-    loadFromImageQuadTc(texture, quad, settings)
-
-    local x, y, w, h = 0, 0, texture:getDimensions()
-    if quad then
-        x, y, w, h = quad:getViewport()
-    end
-
-    local templating = false
-    local imagedata = nil
-    local tile = false
-
-    settings = settings or {}
-    if settings.template then
-        imagedata = settings.template
-        templating = true
-    end
-
-    tile = settings.stretchType == "repeat"
-
-    -- Do not alter subregion when using a template
-    local subregion = nil
-    if templating then
-        local tw, th = imagedata:getDimensions()
-        if (tw - 2) ~= w or (th - 2) ~= h then
-            umg.melt("invalid template dimensions. Expected "..tw.."x"..th..", got "..w.."x"..h.." (note, template dimensions must be 2px larger than the quad)")
-        end
-
-        subregion = {x = x, y = y, w = w, h = h}
-    else
-        subregion = {x = x + 1, y = y + 1, w = w - 2, h = h - 2}
-    end
-
-    if not imagedata then
-        if typecheck.isType(texture, "love:Texture") then
-            ---@cast texture love.Texture
-            -- FIXME: This is slow. Is it cheaper to just re-load the ImageData?
-            imagedata = love.graphics.readbackTexture(texture, nil, 1, x, y, w, h)
-        else
-            ---@cast texture love.ImageData
-            imagedata = love.image.newImageData(w, h, texture:getFormat())
-            imagedata:paste(texture, 0, 0, x, y, w, h)
-        end
-    end
-
-    local obj
-
-    return n9p.loadFromImage(imagedata, {
-        texture = texture,
-        tile = tile,
-        subregion = subregion
-    })
-end
-
----@param name string Name of the assets. Must be available in `client.assets.images`.
----@param settings n9slice.settings? Additional settings. See the `n9slice.settings` type for more information.
----@return n9p.Instance
-function n9slice.loadFromAssets(name, settings)
-    local n9pInstance = n9pObjects[name]
-
-    if not n9pInstance then
-        local quad = client.assets.images[name]
-        if not quad then
-            umg.melt("assets '"..name.."' does not exist")
-        end
-
-        n9pInstance = n9slice.loadFromImageQuad(client.atlas:getTexture(), quad, settings)
-        n9pObjects[name] = n9pInstance
-    end
-
-    return n9pInstance
-end
 
 umg.expose("n9slice", n9slice)
 return n9slice
