c88466a6919033476889ecf133520e9331825da4
Oli <OliverWDGarrett@gmail.com>
2024-08-22 17:37:52 +1200

    Fixed diamond slot

--- lootplot.content.s0/entities/slots/diamond_slot.lua
+++ lootplot.content.s0/entities/slots/diamond_slot.lua
@@ -2,12 +2,24 @@
 return lp.defineSlot("lootplot.content.s0:diamond_slot", {
     image = "diamond_slot",
     name = localization.localize("Diamond slot"),
-    description = localization.localize("Activates items twice"),
+    description = localization.localize("Activates three times"),
     onActivate = function(ent)
-        local item = lp.slotToItem(ent)
-        if item then
-            lp.tryTriggerEntity("PULSE", ent)
-        end
+        local ppos = lp.getPos(ent)
+        if not ppos then return end
+
+        lp.queue(ppos, function ()
+            local item = lp.slotToItem(ent)
+            if item then
+                lp.tryTriggerEntity("PULSE", item)
+            end
+        end)
+        
+        lp.queue(ppos, function ()
+            local item = lp.slotToItem(ent)
+            if item then
+                lp.tryTriggerEntity("PULSE", item)
+            end
+        end)
     end
 })
 
