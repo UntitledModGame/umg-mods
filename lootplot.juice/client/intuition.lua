b3f5cc0997db8a7e072071e3601b590ec4fbf1ff
Miku AuahDark <lumi.hotaru@gmail.com>
2024-08-22 08:38:35 +0800

    Use rendering.drawImage.

--- lootplot.juice/client/intuition.lua
+++ lootplot.juice/client/intuition.lua
@@ -4,19 +4,13 @@ local function entityHasPosition(ent)
     return ent.x and ent.y
 end
 
----@param quad love.Quad
-local function drawQuadInCenterOfEntity(ent, quad)
-    local w, h = select(3, quad:getViewport())
-    client.atlas:draw(quad, ent.x, ent.y, 0, 1, 1, w / 2, h / 2)
-end
-
 umg.on("rendering:drawEntity", RENDER_AFTER_ENTITY_ORDER, function(ent)
     if entityHasPosition(ent) and ent.doomCount then
         if lp.isItemEntity(ent) then
             if ent.doomCount <= 1 then
-                drawQuadInCenterOfEntity(ent, client.assets.images.doom_count_visual)
+                rendering.drawImage(client.assets.images.doom_count_visual, ent.x, ent.y)
             elseif ent.doomCount < 5 then
-                drawQuadInCenterOfEntity(ent, client.assets.images.doom_count_warning_visual)
+                rendering.drawImage(client.assets.images.doom_count_warning_visual, ent.x, ent.y)
             end
         end
         -- TODO: Slot entity cracks.
