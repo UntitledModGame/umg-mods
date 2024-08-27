7eb3e67c2a14a7aeabce377a014321dd53ebcb1b
Oli <OliverWDGarrett@gmail.com>
2024-08-28 11:56:02 +1200

    Creating minerals

---
+++ lootplot.content.s0/entities/items/mineral/mineral.lua
@@ -0,0 +1,15 @@
+
+
+
+local function defineSword(mineral_type, name, powerMult)
+    local etypeName = mineral_type .. "_sword"
+
+    lp.defineItem(etypeName, {
+    })
+end
+
+
+local minerals = {
+
+}
+
