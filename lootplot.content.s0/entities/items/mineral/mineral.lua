4e2d0d02e19d5a7c5206a9a12e5ddac0620a53c1
Oli <OliverWDGarrett@gmail.com>
2024-08-28 14:35:42 +1200

    Created basic minerals

--- lootplot.content.s0/entities/items/mineral/mineral.lua
+++ lootplot.content.s0/entities/items/mineral/mineral.lua
@@ -1,15 +1,60 @@
 
+local loc = localization.localize
 
 
-local function defineSword(mineral_type, name, powerMult)
+local function defineSword(mineral_type, name, tier)
     local etypeName = mineral_type .. "_sword"
+    local image = mineral_type .. "_sword"
 
     lp.defineItem(etypeName, {
+        basePointsGenerated = tier * 5,
+        image = image,
+        name = loc(name .. " Sword"),
+
+        rarity = lp.rarities.COMMON,
+
+        minimumLevelToSpawn = tier,
+        baseBuyPrice = 1 * tier,
     })
 end
 
 
-local minerals = {
 
+local function defineAxe(mineral_type, name, tier)
+    local etypeName = mineral_type .. "_axe"
+    local image = mineral_type .. "_axe"
+
+    lp.defineItem(etypeName, {
+        image = image,
+        name = loc(name .. " Axe"),
+
+        baseMoneyGenerated = 1,
+        targetActivationDescription = loc("{lp_targetColor}Earn points for every target item."),
+
+        rarity = lp.rarities.RARE,
+        minimumLevelToSpawn = 4,
+
+        targetType = "ITEM",
+        targetShape = lp.targets.KNIGHT_SHAPE,
+
+        targetActivate = function(selfEnt, ppos, targetEnt)
+            lp.addPoints(selfEnt, selfEnt.pointsGenerated or 0)
+        end
+    })
+end
+
+
+
+local minerals = {
+    {type = "iron", name = "Iron", tier = 1},
+    {type = "steel", name = "Steel", tier = 2},
+    {type = "ruby", name = "Ruby", tier = 3},
 }
+
+
+for _, t in ipairs(minerals) do
+    defineSword(t.type, t.name, t.tier)
+    defineAxe(t.type, t.name, t.tier)
+end
+
 
