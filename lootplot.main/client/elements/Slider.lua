d9166b0358fc0533e8de56efba7938ec9935469b
Miku AuahDark <lumi.hotaru@gmail.com>
2024-09-10 12:07:22 +0800

    Pull slider from collapsed
    FIXME: Slider is not showing properly.

---
+++ lootplot.main/client/elements/Slider.lua
@@ -0,0 +1,114 @@
+
+local SCROLL_BUTTON = 1
+
+local function getLimitedDelta(elem, mouseX, dx)
+    --[[
+        This is to ensure that the thumb is moved in-tune with
+        the mouse.
+    ]]
+    local x,_y,w,_h = elem:getView()
+    if dx > 0 then
+        -- If mouse is behind elem, and we are dragging forward:
+        if mouseX < x then
+            return 0 -- set delta to 0
+        end
+    else
+        -- If mouse is ahead of elem, and we are dragging back:
+        if mouseX > x+w then
+            return 0 -- set delta to 0
+        end
+    end
+    return dx
+end
+
+local function computeValue(elem, position)
+    -- computes value from position
+    local mag = elem.max - elem.min
+    return elem.min + ((position/elem.totalSize) * mag)
+end
+
+
+
+---@class lootplot.main.SliderThumb: Element
+local Thumb = ui.Element("lootplot.main:SliderThumb")
+
+function Thumb:init(atlas, left, center, right)
+    self.atlas = atlas
+    self.leftQuad = left
+    self.centerQuad = center
+    self.rightQuad = right
+end
+
+
+function Thumb:onPointerMoved(x, y, dx, dy, istouch)
+    if self:isClicked() then
+        local parent = self:getParent()
+        ---@cast parent lootplot.main.Slider
+        dx = getLimitedDelta(self, x, dx)
+        parent.position = math.clamp(parent.position + dx, 0, parent.totalSize)
+        parent.value = computeValue(parent, parent.position)
+        if parent.onValueChanged then
+            parent:onValueChanged(parent.value)
+        end
+    end
+end
+
+function Thumb:onRender(x,y,w,h)
+    ui.style:rectangle(x,y,w,h)
+end
+
+
+
+
+---@class lootplot.main.Slider: Element
+local Slider = ui.Element("lootplot.main:Slider")
+
+function Slider:init(args)
+    typecheck.assertKeys(args, {"onValueChanged", "min", "max"})
+    self.onValueChanged = args.onValueChanged
+    self.min = args.min
+    self.max = args.max
+    assert(self.min<=self.max,"wot wot")
+    self.value = math.clamp(args.value or 0, self.min, self.max)
+    self.valueNormalized = (self.value - self.min) / (self.max - self.min)
+    self.lastWidth = 100
+    self.lastX = 0
+
+    -- make n9slice
+    self.scrollBarSlice = n9slice.new({
+        image = client.atlas:getTexture(),
+        padding = {4, 3},
+        quad = client.assets.images.scrollbar
+    })
+    self.handleImage = client.assets.images.handle_4
+end
+
+
+function Slider:onPointerMoved(x, y, dx, dy, istouch)
+    if self:isClicked() then
+        local clampedX = math.clamp(x, self.lastX, self.lastX + self.lastWidth)
+        self.valueNormalized = (clampedX - self.lastX) / self.lastWidth
+        self.value = (1 - self.valueNormalized) * self.min + self.valueNormalized * self.max
+        if self.onValueChanged then
+            self:onValueChanged(self.value)
+        end
+    end
+end
+
+
+local THUMB_RATIO = 4
+
+
+function Slider:onRender(x,y,w,h)
+    self.lastWidth = w
+    self.lastX = x
+
+    -- FIXME: This slider has **fixed** height!
+    self.scrollBarSlice:draw(x, y + (w - 7 * THUMB_RATIO) / 2, w / THUMB_RATIO, 7, 0, THUMB_RATIO, THUMB_RATIO)
+
+    local handleMaxValue = math.max(w - select(3, self.handleImage:getViewport()) * THUMB_RATIO, 1)
+    client.atlas:draw(self.handleImage, x + self.valueNormalized * handleMaxValue, y, 0, THUMB_RATIO, THUMB_RATIO)
+end
+
+
+return Slider
