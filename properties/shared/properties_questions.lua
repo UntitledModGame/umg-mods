4655ce742f588f1844e898e47085ff96daf81e71
Oli <OliverWDGarrett@gmail.com>
2024-09-10 16:49:47 +1200

    Fixed property syncing, nerfed king ring

--- properties/shared/properties_questions.lua
+++ properties/shared/properties_questions.lua
@@ -4,14 +4,16 @@ umg.defineQuestion("properties:getPropertyMultiplier", reducers.MULTIPLY)
 umg.defineQuestion("properties:getPropertyModifier", reducers.ADD)
 
 local max, min = math.max, math.min
+local INF = math.huge
+
 local function clampCombiner(min1, min2, max1, max2)
     --[[
     Clamps: (min,max)
     --> (2, 5) (0, 4) (1, 3)
     Should output --> (2,3)
     ]]
-    min1 = max(min1, min2)
-    max1 = min(max1, max2)
+    min1 = max(min1 or -INF, min2 or -INF)
+    max1 = min(max1 or INF, max2 or INF)
     return min1, max1
 end
 
