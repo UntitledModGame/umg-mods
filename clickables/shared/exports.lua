c55e5f0cff358592cabeba369a858a5f6fe2e3d2
Miku AuahDark <lumi.hotaru@gmail.com>
2024-09-12 11:42:52 +0800

    Separate clickable logic and the exports file.

---
+++ clickables/shared/exports.lua
@@ -0,0 +1,20 @@
+---@meta
+local clickables = {}
+if false then _G.clickables = clickables end
+
+local c = require("shared.clickable")
+
+if client then
+
+---Get clickables input listener.
+---
+---Availability: **Client**
+---@return input.InputListener
+function clickables.getListener()
+    return c.listener
+end
+
+end
+
+umg.expose("clickables", clickables)
+return clickables
