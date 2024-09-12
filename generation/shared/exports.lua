cc9da0069d7c3ea4373d5b5e43dde4fc64d54a80
Miku AuahDark <lumi.hotaru@gmail.com>
2024-09-12 14:31:07 +0800

    Revert exports doesn't contain logic.

--- generation/shared/exports.lua
+++ generation/shared/exports.lua
@@ -25,7 +25,30 @@ local simplePicker = require("shared.simple_picker")
 ---@param rng love.RandomGenerator? Random number generator to use.
 ---@return T
 function generation.pickWeighted(itemsAndWeights, rng)
-    return simplePicker.pickWeighted(itemsAndWeights, rng)
+    local weightSum = 0
+
+    for _, itemAndWeight in ipairs(itemsAndWeights) do
+        assert(itemAndWeight[2] > 0, "weight must be positive larger than 0")
+        weightSum = weightSum + itemAndWeight[2]
+    end
+
+    local number
+    if rng then
+        number = rng:random()
+    else
+        number = math.random()
+    end
+    number = number * weightSum
+
+    for _, itemAndWeight in ipairs(itemsAndWeights) do
+        number = number - itemAndWeight[2]
+        if number <= 0 then
+            return itemAndWeight[1]
+        end
+    end
+
+    umg.melt("internal error")
+    return nil
 end
 
 ---Randomly picks an item from the list.
@@ -39,7 +62,30 @@ end
 ---@param rng love.RandomGenerator? Random number generator to use.
 ---@return T
 function generation.pickWeightedPlanar(items, weights, rng)
-    return simplePicker.pickWeightedPlanar(items, weights, rng)
+    local weightSum = 0
+
+    for _, weight in ipairs(weights) do
+        assert(weight > 0, "weight must be positive larger than 0")
+        weightSum = weightSum + weight
+    end
+
+    local number
+    if rng then
+        number = rng:random()
+    else
+        number = math.random()
+    end
+    number = number * weightSum
+
+    for i, weight in ipairs(weights) do
+        number = number - weight
+        if number <= 0 then
+            return items[i]
+        end
+    end
+
+    umg.melt("internal error")
+    return nil
 end
 
 umg.expose("generation", generation)
