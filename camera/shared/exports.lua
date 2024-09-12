5b4aaf8f7fa6da8b8fa3882323be341f7a5013d1
Miku AuahDark <lumi.hotaru@gmail.com>
2024-09-12 11:35:29 +0800

    Minor documentation addition to camera.

--- camera/shared/exports.lua
+++ camera/shared/exports.lua
@@ -10,11 +10,16 @@ local DEFAULT_ZOOM = 3
 local globalCamera = Camera(0, 0, nil, nil, DEFAULT_ZOOM)
 
 ---Get the highest priority camera.
+---
+---Availability: Client and Server
 ---@return camera.Camera
 function camera.get()
     return umg.ask("camera:getCamera")
 end
 
+---The Camera class. Use this to create your own camera.
+---
+---Availability: Client and Server
 camera.Camera = Camera
 
 if client then
