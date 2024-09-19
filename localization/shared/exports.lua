a57c2194591d4cd15957b775958f44801700488d
Miku AuahDark <lumi.hotaru@gmail.com>
2024-09-19 14:42:10 +0800

    Use new save/load API for the localization export.
    The downside is the localized strings will be passed from client to server. Rectify this in the future once we have clinet-side saves.

--- localization/shared/exports.lua
+++ localization/shared/exports.lua
@@ -7,6 +7,8 @@ local interpolate = require("shared.interpolate")
 local stringsToLocalize = {}
 ---@type table<string, fun(text:string,variables?:table<string,any>,args?:table)>
 local localizers = {}
+local EXPORT_ON_EXIT = true
+
 
 
 ---Create new localizer for this specific mod.
@@ -32,6 +34,9 @@ function localization.newLocalizer()
             stringsToLocalize[modname] = {}
         end
 
+        if EXPORT_ON_EXIT and not stringsToLocalize[modname][text] and client then
+            client.send("localization:cache", modname, text)
+        end
         stringsToLocalize[modname][text] = text
 
         return variables and interpolate(text, variables) or text
@@ -45,16 +50,29 @@ end
 ---@deprecated use localization.newLocalizer() instead
 localization.localize = localization.newLocalizer()
 
-function localization.exportAll()
-    -- TODO: Support client.
-    if server then
-        local result, jsondata = pcall(server.load, "localization.json")
-        local strings
+-- TODO: Rectify this code once we have client-side saving.
+if EXPORT_ON_EXIT then
 
-        if result then
-            strings = json.decode(jsondata)
-        else
-            strings = {}
+umg.definePacket("localization:cache", {typelist = {"string", "string"}})
+
+if server then
+    server.on("localization:cache", function(_, modname, text)
+        if not stringsToLocalize[modname] then
+            stringsToLocalize[modname] = {}
+        end
+        stringsToLocalize[modname][text] = text
+    end)
+
+    umg.on("@quit", function()
+        local fsobj = server.getSaveFilesystem()
+        local jsondata = fsobj:read("localization.json")
+        local strings = {}
+
+        if jsondata then
+            local res, strs = pcall(json.decode, jsondata)
+            if res then
+                strings = strs
+            end
         end
 
         for modname, stringlist in pairs(stringsToLocalize) do
@@ -68,9 +86,10 @@ function localization.exportAll()
         end
 
         jsondata = json.encode(strings)
-        server.save("localization.json", jsondata)
-    end
+        fsobj:write("localization.json", jsondata)
+    end)
 end
 
+end
 
 umg.expose("localization", localization)
