d32d54124d517f3c9cc0ad49c8f8821303847310
Oli <OliverWDGarrett@gmail.com>
2024-09-20 16:11:33 +1200

    Added calling-cat images

--- lootplot.content.s0/entities/items/contraptions/contraptions.lua
+++ lootplot.content.s0/entities/items/contraptions/contraptions.lua
@@ -14,11 +14,14 @@ https://derekyu.com/makegames/risk.html
 EG:
 Reroll-contraption: Rerolls everything in a KING-2 shape. Doomed-6.
 
-Bomb-contraption: Rerolls everything in a KING-2 shape. 6 uses.
+Bomb-contraption: Destroys everything in a KING-1 shape. Doomed-2.
+
+Activate-contraption: Activates everything in a ABOVE-1 shape. 4 uses.
 
 Remote-contraption: Rerolls everything in a KING-2 shape. 6 uses.
 
-Nullifying-contraption: disables/enables all target items
+Nullifying-contraption: disables/enables all target items/slots
+NOTE::: This can be used to LOCK the shop!!! Very cool idea!!!
 
 ]]
 
