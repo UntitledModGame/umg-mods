4e7f6ad442e80ec526ed4941957d1bedcf810e9e
Oli <OliverWDGarrett@gmail.com>
2024-09-27 16:04:31 +1200

    Created money-limit slot

---
+++ lootplot.content.s0/entities/slots/money_limit_slot.lua
@@ -0,0 +1,25 @@
+
+
+local loc = localization.newLocalizer()
+
+-- Adjust this when balancing. Maybe 100 is better?
+local MONEY_LIMIT = 50
+
+return lp.defineSlot("lootplot.content.s0:money_limit_slot", {
+    image = "money_limit_slot",
+    name = loc("Money-limit slot"),
+    description = loc(("Limits money to %d"):format(MONEY_LIMIT)),
+
+    baseCanSlotPropagate = false,
+    canAddItemToSlot = function()
+        return false -- cant hold items!!!
+    end,
+
+    triggers = {"PULSE"},
+    onActivate = function(ent)
+        local money = lp.getMoney(ent)
+        if money > MONEY_LIMIT then
+            lp.setMoney(ent, MONEY_LIMIT)
+        end
+    end
+})
