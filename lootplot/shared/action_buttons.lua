3d8345165e0192a1e09253b36f21f5258c34ef7b
Oli <OliverWDGarrett@gmail.com>
2024-09-29 15:25:22 +1300

    Work on actionButtons component; nyi

--- lootplot/shared/action_buttons.lua
+++ lootplot/shared/action_buttons.lua
@@ -1,16 +1,76 @@
 
--- actionButton component
+-- actionButtons component
+--[[
 
+
+PLANNING:
+
+ent.actionButtons = {
+    {
+        action = function(ent)
+            -- runs on server AND client.
+            ...
+        end,
+        text = text,
+        color = color
+    }, 
+
+    {
+        ...
+    }
+}
+
+
+]]
+
+
+umg.definePacket("lootplot:actionButtonPress", {
+    typelist = {"entity", "number"}
+})
+
+
+
+local PRIO = 1000 -- we want these action buttons to appear at the RIGHT
+
+local function makeActionButton(ent, index)
+    local actionButton = ent.actionButtons[index]
+    return {
+        text = actionButton.text,
+        color = actionButton.color or objects.Color.WHITE,
+        onClick = function()
+            if not umg.exists(ent) then
+                return -- no-op
+            end
+            -- else, we try sync the operation
+        end,
+        priority = index + PRIO
+    }
+end
+
+
+local function tryPopulateActionButtons(array, ent)
+    umg.melt("NYI.")
+    if not lp.canPlayerAccess(ent, client.getClient()) then
+        return
+    end
+
+    for i, _ in ipairs(ent.actionButtons) do
+        array:add(makeActionButton(ent, i))
+    end
+end
 
 
 -- Custom buttons:
 umg.answer("lootplot:collectSelectionButtons", function(array, ppos)
-    return {
-        text = "Cancel",
-        color = objects.Color(0.66,0.2,0.27),
-        onClick = selection.reset,
-        priority = math.huge
-    }
+    local slotEnt = lp.posToSlot(ppos)
+    if slotEnt and slotEnt.actionButtons then
+        tryPopulateActionButtons(array, slotEnt)
+    end
+
+    local itemEnt = lp.posToSlot(ppos)
+    if itemEnt and itemEnt.actionButtons then
+        tryPopulateActionButtons(array, itemEnt)
+    end
 end)
 
 
