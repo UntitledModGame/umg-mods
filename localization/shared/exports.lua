1d11d07a8e4ab33cca1319ba35bb5612ac644eee
Miku AuahDark <lumi.hotaru@gmail.com>
2024-09-30 11:35:03 +0800

    Make localization text retrieval only usable t load-time.

--- localization/shared/exports.lua
+++ localization/shared/exports.lua
@@ -5,50 +5,80 @@ local interpolate = require("shared.interpolate")
 
 ---@type table<string, table<string, string>>
 local stringsToLocalize = {}
----@type table<string, fun(text:string,variables?:table<string,any>,args?:table)>
-local localizers = {}
+---@type table<string, localization.Interpolator>
+local interpolators = {}
 local EXPORT_ON_EXIT = true
 
 
 
----Create new localizer for this specific mod.
-function localization.newLocalizer()
-    local loadingContext = assert(umg.getLoadingContext(), "this can only be called at load-time")
-    local modname = loadingContext.modname
-    if localizers[modname] then
-        return localizers[modname]
+---@class localization.Interpolator: objects.Class
+local Interpolator = objects.Class("localization:Interpolator")
+
+---@param modname string
+---@param text string
+function Interpolator:init(modname, text, context)
+    self.modname = modname
+    self.text = text
+
+    --[[
+    dummy for now.
+    In future, add proper translation
+    ]]
+    if not stringsToLocalize[modname] then
+        stringsToLocalize[modname] = {}
     end
 
-    --- Translates a string.
-    ---@param text string String to translate
-    ---@param variables table<string, any>? Variable to interpolate
-    ---@param args table? Reserved for future use
-    ---@return string
-    ---@deprecated use localization.newLocalizer instead.
-    local function localize(text, variables, args)
-        --[[
-        dummy for now.
-        In future, add proper translation
-        ]]
-        if not stringsToLocalize[modname] then
-            stringsToLocalize[modname] = {}
-        end
-
-        if EXPORT_ON_EXIT and not stringsToLocalize[modname][text] and client then
-            client.send("localization:cache", modname, text)
-        end
-        stringsToLocalize[modname][text] = text
-
-        return variables and interpolate(text, variables) or text
+    if EXPORT_ON_EXIT and not stringsToLocalize[modname][text] and client then
+        client.send("localization:cache", modname, text)
     end
+    stringsToLocalize[modname][text] = text
+end
 
-    localizers[modname] = localize
-    return localize
+---@param variables table<string, any>? Variable to interpolate
+function Interpolator:__call(variables)
+    return variables and interpolate(self.text, variables) or self.text
+end
+
+function Interpolator:__tostring()
+    return self.text
 end
 
 
----@deprecated use localization.newLocalizer() instead
-localization.localize = localization.newLocalizer()
+
+---@param text string String to translate
+---@param context table? Reserved for future use
+---@return localization.Interpolator|fun(variables:table<string,any>?):string
+function localization.newInterpolator(text, context)
+    local loadingContext = assert(umg.getLoadingContext(), "this can only be called at load-time")
+    local key = loadingContext.modname.."\0"..text
+    local interpolator = interpolators[key]
+
+    if not interpolator then
+        interpolator = Interpolator(loadingContext.modname, text)
+        interpolators[key] = interpolator
+    end
+
+    return interpolator
+end
+
+
+---Create new localizer for this specific mod.
+---@deprecated replaced by `localization.newInterpolator` and `localization.localize`.
+function localization.newLocalizer()
+    return localization.localize
+end
+
+
+--- Translates a string.
+---@param text string String to translate
+---@param variables table<string, any>? Variable to interpolate
+---@param context table? Reserved for future use
+---@return string
+function localization.localize(text, variables, context)
+    return localization.newInterpolator(text, context)(variables)
+end
+
+
 
 -- TODO: Rectify this code once we have client-side saving.
 if EXPORT_ON_EXIT then
