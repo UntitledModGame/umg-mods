03f5086bebc9a45ca9c886ff928b72af281d4a93
Miku AuahDark <lumi.hotaru@gmail.com>
2024-10-03 10:31:46 +0800

    Add NLay 2.

--- layout/shared/exports.lua
+++ layout/shared/exports.lua
@@ -5,7 +5,14 @@ if false then
     _G.layout = layout
 end
 
+
+
+-- Exports
+
 layout.Region = require("lib.kirigami.Region")
+layout.NLay = require("lib.nlay")
+
+-- TODO: Allow sending region/constraint over the network? Do metatable hack + umg.register here?
 
 umg.expose("layout", layout)
 return layout
