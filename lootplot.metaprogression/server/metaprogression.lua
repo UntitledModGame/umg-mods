458a026c73c29bd88bddb68ce90fafb28a10d5a5
Oli <OliverWDGarrett@gmail.com>
2024-10-07 17:11:26 +1300

    Unlock system is working properly

--- lootplot.metaprogression/server/metaprogression.lua
+++ lootplot.metaprogression/server/metaprogression.lua
@@ -51,11 +51,17 @@ end
 local function setValue(storage, name, value)
     local namespace, str = fromNamespaced(name)
     local saveTabl = getSaveTable(storage, namespace)
+    if saveTabl[str] == value then
+        return false
+    end
     saveTabl[str] = value
     local data = json.encode(saveTabl)
     local fsys = server.getSaveFilesystem()
-    local ok, err = fsys:write(getFname(storage, namespace), data)
-    if not ok then
+    local fname = getFname(storage, namespace)
+    local ok, err = fsys:write(fname, data)
+    if ok then
+        umg.log.debug("Saved file: ", fname, " with key-value: ", str, value)
+    else
         umg.log.error(err)
     end
 end
@@ -151,9 +157,9 @@ function lp.metaprogression.winAndUnlockItems(plot)
     for _name, etype in pairs(server.entities) do
         if not seen[etype] then
             seen[etype]=true
-        end
-        if seen.unlock then
-            unlockBuffer:add(etype)
+            if etype.unlock then
+                unlockBuffer:add(etype)
+            end
         end
     end
 
