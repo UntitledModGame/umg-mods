30c50f908a01da3609219746672201a2adec8530
Oli <OliverWDGarrett@gmail.com>
2024-10-17 19:59:38 +1300

    Removed accidental buffs from normal slot

--- lootplot.s0.content/entities/slots/slot.lua
+++ lootplot.s0.content/entities/slots/slot.lua
@@ -1,13 +1,6 @@
 local loc = localization.localize
 
 return lp.defineSlot("lootplot.s0.content:slot", {
-    init = function(ent)
-        if math.random()<0.5 then
-            lp.modifierBuff(ent, "pointsGenerated", 5)
-        else
-            lp.modifierBuff(ent, "pointsGenerated", -5)
-        end
-    end,
     image = "slot",
     name = loc("Basic Slot")
 })
