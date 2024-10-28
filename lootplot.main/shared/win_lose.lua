05bddf297fddc5fa7cf76644256f55da44ceb474
Miku AuahDark <lumi.hotaru@gmail.com>
2024-10-28 11:44:16 +0800

    Collect analytics on win or lose.

--- lootplot.main/shared/win_lose.lua
+++ lootplot.main/shared/win_lose.lua
@@ -7,15 +7,23 @@ if server then
 ---@param clientId string|nil
 ---@param win boolean
 function winLose.endGame(clientId, win)
+    local run = assert(lp.main.getRun())
+
     if win then
-        local plot = assert(lp.main.getRun()):getPlot()
+        local plot = run:getPlot()
         lp.metaprogression.winAndUnlockItems(plot)
     end
+
     if clientId then
         server.unicast(clientId, "lootplot.main:gameEnded", win)
     else
         server.broadcast("lootplot.main:gameEnded", win)
     end
+
+    umg.analytics.collect("lootplot.main:endGame", {
+        win = not not win,
+        runMeta = run:getMetadata()
+    })
 end
 
 else
