4dbb1395066e8c0be430f76daa2b6837b7d11c8a
Oli <OliverWDGarrett@gmail.com>
2024-10-29 15:17:42 +1300

    Changed golden-slot

--- lootplot.s0.content/entities/slots/gold_slot.lua
+++ lootplot.s0.content/entities/slots/gold_slot.lua
@@ -1,13 +1,9 @@
 local loc = localization.localize
 
---[[
 
-not yet implemented!!!
-
-]]
 return lp.defineSlot("lootplot.s0.content:golden_slot", {
     image = "golden_slot",
     name = loc("Golden slot"),
-    description = loc("Can hold %{displayString} items!", lp.rarities.LEGENDARY),
+    baseMoneyGenerated = 1,
 })
 
