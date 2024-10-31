97e1764cec7e58fd3fc81e0f2f589e85926d0239
Oli <OliverWDGarrett@gmail.com>
2024-10-31 16:42:08 +1300

    Made null-slot have no triggers (avoids confusion)

--- lootplot.s0.content/entities/slots/null_slot.lua
+++ lootplot.s0.content/entities/slots/null_slot.lua
@@ -1,6 +1,7 @@
 local loc = localization.localize
 
 return lp.defineSlot("lootplot.s0.content:null_slot", {
+    triggers = {},
     image = "null_slot",
     name = loc("Null slot"),
     description = loc("Doesn't activate items!"),
