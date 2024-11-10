ecf8de0edbd81b28021cde1611ed7a9905857a97
Oli <OliverWDGarrett@gmail.com>
2024-11-11 11:53:38 +1300

    Made tier-upgrade description a biiit clearer

--- lootplot.tiers/client/tier_upgrade_desc.lua
+++ lootplot.tiers/client/tier_upgrade_desc.lua
@@ -12,7 +12,7 @@ umg.on("lootplot:populateDescription", 70, function(ent, arr)
     local selected = lp.getCurrentSelection()
     local item = selected and selected.item
     if item and item ~= ent and lp.tiers.canUpgrade(ent, item) then
-        arr:add("{lootplot:COMBINE_COLOR}---------------")
+        arr:add("{wavy amp=2}{lootplot:COMBINE_COLOR}==========================")
         arr:add("{lootplot:COMBINE_COLOR}{wavy}UPGRADE:")
         local desc = ent.tierUpgrade.description
         if objects.isCallable(desc) then
@@ -20,7 +20,7 @@ umg.on("lootplot:populateDescription", 70, function(ent, arr)
         end
         assert(type(desc) == "string", "?")
         arr:add("{lootplot:COMBINE_COLOR}  " .. desc)
-        arr:add("{lootplot:COMBINE_COLOR}---------------")
+        arr:add("{wavy amp=2}{lootplot:COMBINE_COLOR}==========================")
     end
 end)
 
