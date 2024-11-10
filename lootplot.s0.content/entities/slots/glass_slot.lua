2c3aa24c5ba0b298068c0f059cf06492895a1ddd
Oli <OliverWDGarrett@gmail.com>
2024-11-11 12:39:10 +1300

    Buffed glass slots

--- lootplot.s0.content/entities/slots/glass_slot.lua
+++ lootplot.s0.content/entities/slots/glass_slot.lua
@@ -12,9 +12,9 @@ end
 return lp.defineSlot("lootplot.s0.content:glass_slot", {
     image = "glass_slot",
     name = loc("Glass slot"),
-    description = loc("Has a 20% chance of being destroyed when activated"),
+    description = loc("Has a 10% chance of being destroyed when activated"),
     onActivate = function(ent)
-        if math.random() < 0.2 then
+        if math.random() < 0.1 then
             -- WELP! riparoni pepperoni
             lp.destroy(ent)
         end
