6843e18ecd47b0c223b9ac000aa4f89a57dc390e
Oli <OliverWDGarrett@gmail.com>
2024-11-11 13:30:33 +1300

    Added pineapple ring item

--- lootplot.s0.content/entities/items/money_scavenger/scavenger.lua
+++ lootplot.s0.content/entities/items/money_scavenger/scavenger.lua
@@ -1,12 +1,33 @@
 
+--[[
+===================================================
+
+GRUBBY ITEMS:
+Items that work well when low on money.
+
+Uses `grubMoneyCap` component.
+
+
+NOTE:
+our systems support money-caps of any value;
+but, in order to keep it simple; we are having only 2 types:
+- money-cap
+
+===================================================
+]]
+
 local loc = localization.localize
 local interp = localization.newInterpolator
 local helper = require("shared.helper")
 
 
+local MONEY_CAP_LOW = 5
+local MONEY_CAP_MID = 10
+
+
 local function defItem(id, etype)
     etype.image = etype.image or id
-    etype.grubMoneyCap = etype.grubMoneyCap or 15
+    etype.grubMoneyCap = etype.grubMoneyCap or MONEY_CAP_MID
     return lp.defineItem("lootplot.s0.content:"..id, etype)
 end
 
@@ -37,7 +58,7 @@ defItem("spare_coins", {
     name = loc("Spare Coins"),
     triggers = {"PULSE"},
 
-    grubMoneyCap = 6,
+    grubMoneyCap = MONEY_CAP_LOW,
 
     basePrice = 6,
     baseMoneyGenerated = 1,
@@ -45,4 +66,30 @@ defItem("spare_coins", {
 
     rarity = lp.rarities.UNCOMMON,
 })
+
+
+
+defItem("pineapple_ring", {
+    name = loc("Pineapple Ring"),
+
+    basePrice = 8,
+    doomCount = 8,
+    grubMoneyCap = MONEY_CAP_MID,
+    canItemFloat = true,
+    baseMoneyGenerated = 2,
+
+    triggers = {},
+    listen = {
+        trigger = "BUY",
+        activate = function(selfEnt, ppos, targetEnt)
+            local x = math.floor( (targetEnt.price or 2)/2 + 0.5 )
+            lp.addMoney(selfEnt, x)
+        end,
+    },
+
+    shape = lp.targets.CircleShape(2),
+
+    rarity = lp.rarities.UNCOMMON,
+})
+
 
