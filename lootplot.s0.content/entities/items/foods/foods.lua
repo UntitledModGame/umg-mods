7a1eedcd6642e54ff7a9ad20762f7cc0cbb3eff0
Oli <OliverWDGarrett@gmail.com>
2024-11-13 15:34:04 +1300

    Swapped rarities of KING and KNIGHTS pies

--- lootplot.s0.content/entities/items/foods/foods.lua
+++ lootplot.s0.content/entities/items/foods/foods.lua
@@ -428,8 +428,8 @@ local function definePie(id, name, desc, addShape, rarity)
     })
 end
 
-definePie("knights_pie", "Knight's Pie", "Adds KNIGHT Shape to item", lp.targets.KNIGHT_SHAPE, lp.rarities.UNCOMMON)
-definePie("kings_pie", "King's Pie", "Adds KING-1 Shape to item", lp.targets.KingShape(1), lp.rarities.RARE)
+definePie("kings_pie", "King's Pie", "Adds KING-1 Shape to item", lp.targets.KingShape(1), lp.rarities.UNCOMMON)
+definePie("knights_pie", "Knight's Pie", "Adds KNIGHT Shape to item", lp.targets.KNIGHT_SHAPE, lp.rarities.RARE)
 definePie("rooks_pie", "Rook's Pie", "Adds ROOK-4 Shape to item", lp.targets.RookShape(4), lp.rarities.EPIC)
 
 
