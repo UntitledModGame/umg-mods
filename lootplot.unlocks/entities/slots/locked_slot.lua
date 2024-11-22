669052b53d271f91cf46c905c7fc3d31e9dcba0e
Miku AuahDark <lumi.hotaru@gmail.com>
2024-11-22 08:22:03 +0800

    Define locked slot and the spawn helpers.

--- lootplot.unlocks/entities/slots/locked_slot.lua
+++ lootplot.unlocks/entities/slots/locked_slot.lua
@@ -1,7 +1,37 @@
+local loc = localization.localize
 
---[[
+return lp.defineSlot("lootplot.unlocks:locked_slot", {
+    image = "locked_slot",
+    name = loc("Locked Slot"),
+    description = loc("{i}What does it contain?{/i}"),
+    triggers = {"UNLOCK"},
+    locked = true,
 
-TODO: put locked slot definition in here
-(Along with the image!)
+    ---@param self lootplot.SlotEntity
+    onActivate = function(self)
+        if self.locked then
+            return
+        end
 
-]]
+        local ppos = assert(lp.getPos(self))
+        local tslot = self.targetSlot
+        local titem = self.targetItem
+
+        self:removeComponent("targetSlot")
+        self:removeComponent("targetItem")
+        -- sync.syncComponent(self, "targetSlot")
+        -- sync.syncComponent(self, "targetItem")
+
+        if tslot then
+            lp.setSlot(ppos, tslot)
+        end
+
+        if titem then
+            if not lp.forceSetItem(ppos, titem) then
+                titem:delete()
+            end
+        end
+
+        lp.destroy(self)
+    end
+})
--- lootplot.unlocks/shared/lp_unlocks.lua
+++ lootplot.unlocks/shared/lp_unlocks.lua
@@ -18,17 +18,40 @@ but also used for chests, and other special items that need unlocking.
 
 if server then
 
-local spawnLockedSlotTc = typecheck.assert("ppos", "entity", "entity")
+local lockedslotEType = require("entities.slots.locked_slot")
 
+local spawnLockedSlotTc = typecheck.assert("ppos", "string", "entity?", "entity?")
+
+---Availability: **Server**
 ---@param ppos lootplot.PPos
----@param transformSlotEnt Entity The slot-entity to be transformed into
----@param lockedItemEnt Entity The item-entity to be transformed into
-function lp.unlocks.spawnLockedSlot(ppos, transformSlotEnt, lockedItemEnt)
-    spawnLockedSlotTc(ppos, transformSlotEnt, lockedItemEnt)
-    --[[
-    TODO: implement this.
-    ]]
+---@param team string
+---@param transformSlotEnt lootplot.SlotEntity? The slot-entity to be transformed into
+---@param lockedItemEnt lootplot.ItemEntity? The item-entity to be transformed into
+function lp.unlocks.forceSpawnLockedSlot(ppos, team, transformSlotEnt, lockedItemEnt)
+    spawnLockedSlotTc(ppos, team, transformSlotEnt, lockedItemEnt)
+    local ent = lp.forceSpawnSlot(ppos, lockedslotEType, team)
+
+    ent.targetSlot = transformSlotEnt
+    ent.targetItem = lockedItemEnt
+    return ent
+end
+
+---Availability: **Server**
+---@param ppos lootplot.PPos
+---@param team string
+---@param transformSlotEnt lootplot.SlotEntity? The slot-entity to be transformed into
+---@param lockedItemEnt lootplot.ItemEntity? The item-entity to be transformed into
+---@return lootplot.SlotEntity?
+function lp.unlocks.trySpawnLockedSlot(ppos, team, transformSlotEnt, lockedItemEnt)
+    spawnLockedSlotTc(ppos, team, transformSlotEnt, lockedItemEnt)
+    local ent = lp.trySpawnSlot(ppos, lockedslotEType, team)
+
+    if ent then
+        ent.targetSlot = transformSlotEnt
+        ent.targetItem = lockedItemEnt
+    end
+
+    return ent
 end
 
 end
-
