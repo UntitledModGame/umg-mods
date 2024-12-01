989d718494758d1275732a805d336b56666a8a05
Oli <OliverWDGarrett@gmail.com>
2024-12-02 12:58:17 +1300

    Created/added rotatey sprites

---
+++ lootplot.s0.content/entities/items/rotatey/rotatey.lua
@@ -0,0 +1,25 @@
+
+--[[
+
+Gear:
+On PULSE,ROTATE:
+Rotates all target items
+KING-1
+
+
+Shuriken:
+On PULSE:
+Earn points
+(When rotated, gain +1 mult)
+
+
+Tumbling cat:
+Same as copycat, but rotates the spawned cat
+shape = UP-2
+
+
+
+Screw: 
+TODO.
+
+]]
