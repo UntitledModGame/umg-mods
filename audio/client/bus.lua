e5b202e9253222340a10a8f2b7f28bee218e822c
Miku AuahDark <lumi.hotaru@gmail.com>
2024-12-03 11:34:04 +0800

    audio: Remove unnecessary qbus answer.

--- audio/client/bus.lua
+++ audio/client/bus.lua
@@ -11,10 +11,6 @@ umg.answer("audio:getVolume", function(_, _, e)
     return e and e.audioVolume or 1
 end)
 
-umg.answer("audio:getSemitoneOffset", function()
-    return 0
-end)
-
 umg.answer("audio:getSemitoneOffset", function(_, _, e)
     return e and e.audioSemitoneOffset or 0
 end)
