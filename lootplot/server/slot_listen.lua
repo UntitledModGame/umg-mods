d6f932db006c79f41abafd9c1382f5afaba0906e
Oli <OliverWDGarrett@gmail.com>
2024-12-03 15:41:35 +1300

    Fixed slotListen component

--- lootplot/server/slot_listen.lua
+++ lootplot/server/slot_listen.lua
@@ -2,7 +2,11 @@ umg.on("lootplot:entityTriggered", function(ent, triggerName)
     if lp.isItemEntity(ent) then
         local slotEnt = lp.itemToSlot(ent)
         if slotEnt and slotEnt.slotListen and slotEnt.slotListen.trigger == triggerName then
-            lp.tryActivateEntity(slotEnt)
+            local success = lp.tryActivateEntity(slotEnt)
+            local sL = slotEnt.slotListen
+            if success and sL.activate then
+                sL.activate(slotEnt)
+            end
         end
     end
 end)
