9d19a216b93ee01f30a267e4026a61eb7ca696fe
Miku AuahDark <lumi.hotaru@gmail.com>
2024-12-04 11:06:17 +0800

    Provide default answer to entity properties.

--- rendering/client/helper/entity_properties.lua
+++ rendering/client/helper/entity_properties.lua
@@ -8,14 +8,14 @@ local umg_ask = umg.ask
 ---@param ent Entity
 ---@return number
 function entityProperties.getRotation(ent)
-    return (ent.rot or 0) + (umg_ask("rendering:getRotation", ent) or 0)
+    return (ent.rot or 0) + umg_ask("rendering:getRotation", ent)
 end
 
 
 ---@param ent Entity
 ---@return number
 function entityProperties.getScale(ent)
-    return (ent.scale or 1) * (umg_ask("rendering:getScale", ent) or 1)
+    return (ent.scale or 1) * umg_ask("rendering:getScale", ent)
 end
 
 
@@ -23,21 +23,21 @@ end
 ---@return number,number
 function entityProperties.getScaleXY(ent)
     local sx, sy = umg_ask("rendering:getScaleXY", ent)
-    return (ent.scaleX or 1) * (sx or 1), (ent.scaleY or 1) * (sy or 1)
+    return (ent.scaleX or 1) * sx, (ent.scaleY or 1) * sy
 end
 
 ---@param ent Entity
 ---@return number,number
 function entityProperties.getOffsetXY(ent)
     local ox, oy = umg_ask("rendering:getOffsetXY", ent)
-    return (ent.ox or 0) + (ox or 0), (ent.oy or 0) + (oy or 0)
+    return (ent.ox or 0) + ox, (ent.oy or 0) + oy
 end
 
 ---@param ent Entity
 ---@return number,number
 function entityProperties.getShearXY(ent)
     local kx, ky = umg_ask("rendering:getShearXY", ent)
-    return (ent.shearX or 0) + (kx or 0), (ent.shearY or 0) + (ky or 0)
+    return (ent.shearX or 0) + kx, (ent.shearY or 0) + ky
 end
 
 
@@ -48,7 +48,7 @@ function entityProperties.getOpacity(ent)
     if ent.color and ent.color[4] then
         a = ent.color[4]
     end
-    return a * (ent.opacity or 1) * (umg_ask("rendering:getOpacity", ent) or 1)
+    return a * (ent.opacity or 1) * umg_ask("rendering:getOpacity", ent)
 end
 
 
@@ -57,9 +57,6 @@ end
 ---@return number,number,number
 function entityProperties.getColor(ent)
     local r,g,b = umg_ask("rendering:getColor", ent)
-    if not r then
-        r,g,b = 1,1,1
-    end
 
     local color = ent.color
     if color then
@@ -72,14 +69,14 @@ end
 ---@param ent Entity
 ---@return string?
 function entityProperties.getImage(ent)
-    return umg_ask("rendering:getImage", ent) or ent.image 
+    return umg_ask("rendering:getImage", ent) or ent.image
 end
 
 
 ---@param ent Entity
 ---@return boolean
 function entityProperties.isHidden(ent)
-    return not not (ent.hidden or umg.ask("rendering:isHidden", ent))
+    return (not not ent.hidden) or (not not umg_ask("rendering:isHidden", ent))
 end
 
 
