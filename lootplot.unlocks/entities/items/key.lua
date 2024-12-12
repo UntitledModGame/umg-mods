128333c44777068cbc874d74c971cd04aa470693
Oli <OliverWDGarrett@gmail.com>
2024-12-12 17:37:22 +1300

    Adjustments to key item

--- lootplot.unlocks/entities/items/key.lua
+++ lootplot.unlocks/entities/items/key.lua
@@ -10,6 +10,28 @@ lp.defineItem("lootplot.unlocks:key", {
 
     triggers = {"PULSE"},
 
+    doomCount = 1,
+
+    canActivate = function(ent)
+        --[[
+        Keys will only activate is there is actually an entity TO activate.
+        (Or else its a silly waste!)
+        ]]
+        local targs = lp.targets.getShapePositions(ent)
+        if not targs then return false end
+        for _,ppos in ipairs(targs) do
+            local item = lp.posToItem(ppos)
+            local slot = lp.posToSlot(ppos)
+            if item and lp.hasTrigger(item, "UNLOCK") then
+                return true
+            end
+            if slot and lp.hasTrigger(slot, "UNLOCK") then
+                return true
+            end
+        end
+        return false
+    end,
+
     shape = lp.targets.VerticalShape(1),
     target = {
         type = lp.CONVERSIONS.ITEM_OR_SLOT,
