6e40ba83fbe54e6e5f113d91d430563e74e6aea9
Oli <OliverWDGarrett@gmail.com>
2024-12-30 19:55:04 +1300

    Nerfed hammers

--- lootplot.s0.content/entities/items/mineral/mineral.lua
+++ lootplot.s0.content/entities/items/mineral/mineral.lua
@@ -171,7 +171,7 @@ local function defineHammer(mineral_type, name, strength, etype)
         basePrice = 10,
         baseMultGenerated = floorTo01(0.1 * strength),
 
-        shape = lp.targets.HorizontalShape(2),
+        shape = lp.targets.HorizontalShape(1),
 
         activateDescription = function(ent)
             return HAMMER_DESC({
