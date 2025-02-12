e5a94b84b7368184ed42289b54ee2958f25401f4
Oli <OliverWDGarrett@gmail.com>
2025-02-12 14:38:48 +1300

    Adjusted food rarities

--- lootplot.s0/entities/items/foods/foods.lua
+++ lootplot.s0/entities/items/foods/foods.lua
@@ -354,7 +354,7 @@ defineFood("heartfruit_purple", {
     name = loc("Purple Heart Fruit"),
     activateDescription = loc("Gives {lootplot:DOOMED_COLOR}DOOMED-2{/lootplot:DOOMED_COLOR} to target item (or slot)"),
 
-    rarity = lp.rarities.UNCOMMON,
+    rarity = lp.rarities.RARE,
 
     shape = lp.targets.UP_SHAPE,
 
@@ -848,13 +848,13 @@ local function definePie(id, name, desc, addShape, rarity)
     })
 end
 
--- uncommon pies:
-definePie("small_rooks_pie", "Small Rook's Pie", "Adds ROOK-2 Shape to target item", lp.targets.RookShape(2), lp.rarities.UNCOMMON)
-definePie("kings_pie", "King's Pie", "Adds KING-1 Shape to target item", lp.targets.KingShape(1), lp.rarities.UNCOMMON)
-definePie("bishops_pie", "Bishop's Pie", "Adds BISHOP-2 Shape to target item", lp.targets.BishopShape(2), lp.rarities.UNCOMMON)
+-- rare pies:
+definePie("small_rooks_pie", "Small Rook's Pie", "Adds ROOK-2 Shape to target item", lp.targets.RookShape(2), lp.rarities.RARE)
+definePie("kings_pie", "King's Pie", "Adds KING-1 Shape to target item", lp.targets.KingShape(1), lp.rarities.RARE)
+definePie("bishops_pie", "Bishop's Pie", "Adds BISHOP-2 Shape to target item", lp.targets.BishopShape(2), lp.rarities.RARE)
 
--- rare/epic pies:
-definePie("knights_pie", "Knight's Pie", "Adds KNIGHT Shape to target item", lp.targets.KNIGHT_SHAPE, lp.rarities.RARE)
+-- epic pies:
+definePie("knights_pie", "Knight's Pie", "Adds KNIGHT Shape to target item", lp.targets.KNIGHT_SHAPE, lp.rarities.EPIC)
 definePie("rooks_pie", "Rook's Pie", "Adds ROOK-5 Shape to target item", lp.targets.RookShape(4), lp.rarities.EPIC)
 
 
