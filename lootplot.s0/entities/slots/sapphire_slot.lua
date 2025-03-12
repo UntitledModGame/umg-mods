949a701032f70d4db06fb367973d2d71317c3611
Oli <OliverWDGarrett@gmail.com>
2025-03-12 16:23:57 +1300

    Modified sapphire-slot

--- lootplot.s0/entities/slots/sapphire_slot.lua
+++ lootplot.s0/entities/slots/sapphire_slot.lua
@@ -11,7 +11,7 @@ return lp.defineSlot("lootplot.s0:sapphire_slot", {
 
     rarity = lp.rarities.RARE,
 
-    description = loc("While on this slot,\nItems earn {lootplot:POINTS_MULT_COLOR}%{weakMult} x points{/lootplot:POINTS_MULT_COLOR}.\n(If {lootplot:BONUS_COLOR}Bonus{/lootplot:BONUS_COLOR} is negative, becomes {lootplot:POINTS_MULT_COLOR}%{strongMult}x{/lootplot:POINTS_MULT_COLOR})", {
+    description = loc("While on this slot,\nItems earn {lootplot:POINTS_MULT_COLOR}%{weakMult} x points{/lootplot:POINTS_MULT_COLOR}.\n(If {lootplot:BONUS_COLOR}Bonus{/lootplot:BONUS_COLOR} is negative, becomes {lootplot:POINTS_MULT_COLOR}%{strongMult} x{/lootplot:POINTS_MULT_COLOR})", {
         weakMult = WEAK_MULT,
         strongMult = STRONG_MULT,
     }),
