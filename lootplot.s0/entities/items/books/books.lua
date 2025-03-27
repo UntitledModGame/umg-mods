07f1b516f54834f979048ad1a5899ac93e3a6a0f
Oli <OliverWDGarrett@gmail.com>
2025-03-27 13:49:07 +1300

    Gave books an action-button (more agency)

--- lootplot.s0/entities/items/books/books.lua
+++ lootplot.s0/entities/items/books/books.lua
@@ -7,16 +7,29 @@ local helper = require("shared.helper")
 local bookTc = typecheck.assert("string", "string", "string|function", "string", "table")
 
 
+
+local ACTIVATE_SELF_BUTTON = {
+    text = loc("Read!"),
+    action = function(selfEnt)
+        if server then
+            lp.tryActivateEntity(selfEnt)
+        end
+    end
+}
+
+
+
 local function defineBook(id, name, etype)
     etype.image = id
     etype.name = loc(name)
-    etype.triggers = {"PULSE"}
 
     etype.basePrice = 10
     etype.baseMaxActivations = 10
 
     etype.doomCount = 20
     etype.shape = lp.targets.UP_SHAPE
+
+    etype.actionButtons = {ACTIVATE_SELF_BUTTON}
 
     lp.defineItem(  "lootplot.s0:" .. id, etype)
 end
