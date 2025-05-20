cd9dfae50c671a4908d6f61ca506d37e70bf7361
Oli <OliverWDGarrett@gmail.com>
2025-05-20 13:56:13 +1200

    Simplified emerald-slot

--- lootplot.s0/entities/slots/emerald_slot.lua
+++ lootplot.s0/entities/slots/emerald_slot.lua
@@ -6,17 +6,15 @@ return lp.defineSlot("lootplot.s0:emerald_slot", {
     image = "emerald_slot",
     name = loc("Emerald Slot"),
 
-    activateDescription = loc("Triggers {lootplot:TRIGGER_COLOR}PULSE{/lootplot:TRIGGER_COLOR} and {lootplot:TRIGGER_COLOR}REROLL{/lootplot:TRIGGER_COLOR} for item!\n{lootplot:BAD_COLOR}Does not trigger items otherwise."),
+    activateDescription = loc("Triggers {lootplot:TRIGGER_COLOR}Pulse{/lootplot:TRIGGER_COLOR} on item"),
     baseMaxActivations = 100,
     triggers = {"REROLL"},
-    dontPropagateTriggerToItem = true,
 
     rarity = lp.rarities.UNCOMMON,
 
     onActivate = function(slotEnt)
         local item = lp.slotToItem(slotEnt)
         if item then
-            lp.tryTriggerEntity("REROLL", item)
             lp.tryTriggerEntity("PULSE", item)
         end
     end
