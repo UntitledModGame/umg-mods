fce5dd06348473af2483892942baeca8ccf3957c
Oli <OliverWDGarrett@gmail.com>
2025-05-26 17:46:03 +1200

    Made glove descriptions better

--- lootplot.s0/entities/items/glove/glove.lua
+++ lootplot.s0/entities/items/glove/glove.lua
@@ -1,10 +1,18 @@
-local loc = localization.localize
 
-local function defineGlove(id, name, description, giveShape, rarity)
+
+local loc = localization.localize
+local interp = localization.newInterpolator
+
+
+local SHAPE_DESC = interp("Set item's {lootplot.targets:COLOR}targets{/lootplot.targets:COLOR} to %{shapeName}.")
+
+local function defineGlove(id, name, giveShape, rarity)
     return lp.defineItem("lootplot.s0:"..id, {
         image = id,
         name = loc(name),
-        activateDescription = loc(description),
+        activateDescription = SHAPE_DESC({
+            shapeName = giveShape.name
+        }),
 
         rarity = rarity,
 
@@ -31,21 +39,18 @@ end
 
 defineGlove("up_glove",
     "Up Glove",
-    "Set item's targets to UP-1.",
     lp.targets.UpShape(1),
     lp.rarities.RARE
 )
 
 defineGlove("knight_glove",
     "Knight Glove",
-    "Set item's targets to KNIGHT.",
     lp.targets.KNIGHT_SHAPE,
     lp.rarities.EPIC
 )
 
 defineGlove("bishop_glove",
     "Bishop Glove",
-    "Set item's targets to BISHOP-2.",
     lp.targets.BishopShape(2),
     lp.rarities.EPIC
 )
@@ -53,14 +58,12 @@ defineGlove("bishop_glove",
 
 defineGlove("king_glove",
     "King Glove",
-    "Set item's targets to KING-1.",
     lp.targets.KING_SHAPE,
     lp.rarities.RARE
 )
 
 defineGlove("rook_glove",
     "Rook Glove",
-    "Set item's targets to ROOK-6.",
     lp.targets.RookShape(6),
     lp.rarities.LEGENDARY
 )
