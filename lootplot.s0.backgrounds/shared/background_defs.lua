382692956dbf5e3c137aa0286f7e1e9726666c8c
Oli <OliverWDGarrett@gmail.com>
2025-05-27 16:01:36 +1200

    Created crimson and aqua backgrounds

--- lootplot.s0.backgrounds/shared/background_defs.lua
+++ lootplot.s0.backgrounds/shared/background_defs.lua
@@ -9,6 +9,8 @@ local popcornCtor = nil
 local tealCtor = nil
 local voidCtor = nil
 local starCtor = nil
+local aquaCtor = nil
+local crimsonCtor = nil
 
 
 
@@ -119,6 +121,31 @@ function starCtor()
 end
 
 
+function aquaCtor()
+    return CloudBackground({
+        worldX = -W/2 + DELTA, worldY = -H/2 + DELTA,
+        worldWidth = W, worldHeight = H,
+        numberOfClouds = 100,
+
+        backgroundColor = objects.Color("#" .. "FF11FFF7"),
+        cloudColor = objects.Color("#" .. "FF50FFF9"),
+    })
+end
+
+
+function crimsonCtor()
+    return CloudBackground({
+        worldX = -W/2 + DELTA, worldY = -H/2 + DELTA,
+        worldWidth = W, worldHeight = H,
+        numberOfClouds = 0,
+
+        backgroundColor = objects.Color("#" .. "FF400D21"),
+        cloudColor = objects.Color("#" .. "FF400D21"),
+    })
+end
+
+
+
 
 end
 
@@ -135,13 +162,21 @@ lp.backgrounds.registerBackground("lootplot.s0.backgrounds:sunset_background", {
     icon = "sunset_cloud_background"
 })
 
-lp.backgrounds.registerBackground("lootplot.s0.backgrounds:void_background", {
-    name = loc("Void"),
-    constructor = voidCtor,
+local STAR_FOG_COLOR
+do
+local R,G,B = 18, 0, 33
+STAR_FOG_COLOR = {R/255, G/255, B/255}
+end
+lp.backgrounds.registerBackground("lootplot.s0.backgrounds:star_background", {
+    name = loc("Star Background"),
+    description = loc("Just stars"),
+    constructor = starCtor,
     isUnlocked = winToUnlock(),
-    icon = "void_background",
-    fogColor = objects.Color("#" .. "FF250732")
+    icon = "star_background",
+    fogColor = STAR_FOG_COLOR,
 })
+
+
 
 lp.backgrounds.registerBackground("lootplot.s0.backgrounds:cherry_background", {
     name = loc("Cherry"),
@@ -164,18 +199,31 @@ lp.backgrounds.registerBackground("lootplot.s0.backgrounds:popcorn_background", 
     icon = "popcorn_background",
 })
 
-local STAR_FOG_COLOR
-do
-local R,G,B = 18, 0, 33
-STAR_FOG_COLOR = {R/255, G/255, B/255}
-end
-lp.backgrounds.registerBackground("lootplot.s0.backgrounds:star_background", {
-    name = loc("Star Background"),
-    description = loc("Just stars"),
-    constructor = starCtor,
+
+lp.backgrounds.registerBackground("lootplot.s0.backgrounds:void_background", {
+    name = loc("Void"),
+    constructor = voidCtor,
     isUnlocked = winToUnlock(),
-    icon = "star_background",
-    fogColor = STAR_FOG_COLOR,
+    icon = "void_background",
+    fogColor = objects.Color("#" .. "FF250732")
+})
+
+
+lp.backgrounds.registerBackground("lootplot.s0.backgrounds:aqua_background", {
+    name = loc("Aqua"),
+    constructor = aquaCtor,
+    isUnlocked = winToUnlock(),
+    icon = "aqua_cloud_background",
+    fogColor = objects.Color("#" .. "FF0CB4C7")
+})
+
+
+lp.backgrounds.registerBackground("lootplot.s0.backgrounds:crimson_background", {
+    name = loc("Crimson"),
+    constructor = crimsonCtor,
+    isUnlocked = winToUnlock(),
+    fogColor = objects.Color("#" .. "FF0F0003"),
+    icon = "crimson_background",
 })
 
 
