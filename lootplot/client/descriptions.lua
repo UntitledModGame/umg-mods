c78357c93a5e8967b0a24a2894d30f41a3a3a1bb
Oli <OliverWDGarrett@gmail.com>
2025-06-06 19:28:52 +1200

    Fixed invincible description

--- lootplot/client/descriptions.lua
+++ lootplot/client/descriptions.lua
@@ -360,7 +360,7 @@ umg.on("lootplot:populateMetaDescription", 60, function(ent, arr)
 end)
 
 local EXTRA_LIFE = interp("{wavy}{lootplot:LIFE_COLOR}EXTRA LIVES:{/lootplot:LIFE_COLOR} %{lives}")
-local INVINCIBLE = interp("{wavy}{lootplot:INFO_COLOR}INVINCIBLE:{/lootplot:INFO_COLOR} Cannot be destroyed!")
+local INVINCIBLE = interp("{wavy}{lootplot:INFO_COLOR}INVINCIBLE:{/lootplot:INFO_COLOR}{/wavy} Cannot be destroyed!")
 
 umg.on("lootplot:populateMetaDescription", 60, function(ent, arr)
     if ent.lives and ent.lives > 0 then
